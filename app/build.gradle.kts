1ffbf0d80f8be5c7cfd4d86cc6968b8ee1c34bcd
meysam82003 <meysamsalamatnasab@gmail.com>
2026-09-19 11:12:37 +0330

    fix(release): preserve original lifecycle dependency while resetting display version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -130,7 +130,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
-    implementation("androidx.lifecycle:viewmodel-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
     implementation(files("libs/ca.psiphon.aar"))
     implementation("androidx.compose.material:material-icons-extended")
     implementation("dev.chrisbanes.haze:haze:1.6.0")
